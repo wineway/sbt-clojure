ce06e0284c0e30c9e105b9f6945c4a34eb291baf
Yuwen Wang <ywang1@hetao.ai>
2019-05-05 19:57:21 +0800

    modified clojurePlugin

--- src/main/scala/ClojurePlugin.scala
+++ src/main/scala/ClojurePlugin.scala
@@ -1,14 +1,20 @@
 import sbt._
 import sbt.Keys._
 
-
 object ClojurePlugin extends AutoPlugin {
 
   object autoImport {
-    lazy val Clojure = config("clojure") extend(Compile) hide
-    lazy val clojureVersion = SettingKey[String]("clojure-version", "version of clojure to use for building")
-    lazy val clojureSource = SettingKey[File]("clojure-source", "clojure source directory")
-    lazy val clojureCompile = TaskKey[Unit]("clojure-compile", "runs clojure compilation, occurs before normal compilation")
+    lazy val Clojure = config("clojure") extend (Compile) hide
+    lazy val clojureVersion = SettingKey[String](
+      "clojure-version",
+      "version of clojure to use for building"
+    )
+    lazy val clojureSource =
+      SettingKey[File]("clojure-source", "clojure source directory")
+    lazy val clojureCompile = TaskKey[Unit](
+      "clojure-compile",
+      "runs clojure compilation, occurs before normal compilation"
+    )
   }
 
   import autoImport._
@@ -16,13 +22,17 @@ object ClojurePlugin extends AutoPlugin {
   override lazy val projectSettings = Seq(
     libraryDependencies += "org.clojure" % "clojure" % clojureVersion.value,
     clojureSource in Compile := (sourceDirectory in Compile).value / "clojure",
-    unmanagedResourceDirectories in Compile += {(clojureSource in Compile).value},
+    unmanagedResourceDirectories in Compile += {
+      (clojureSource in Compile).value
+    },
     clojureCompile in Compile := {
       val taskStreams: TaskStreams = streams.value.asInstanceOf[TaskStreams]
       val sourceDirectory: File = (clojureSource in Compile).value
       taskStreams.log("compiling clojure source...")
 
-      val classpath: Seq[File] = update.value.select(configurationFilter(name = "*")) ++
+      val classpath: Seq[File] = update.value.select(
+        configurationFilter(name = "*")
+      ) ++
         Seq((classDirectory in Compile).value) ++
         Seq(sourceDirectory)
 
@@ -31,21 +41,31 @@ object ClojurePlugin extends AutoPlugin {
 
       def clojureClazz(file: File): File = {
         val path = file.getAbsolutePath
-        new File(destinationDirectory.getAbsolutePath + path.substring(sourceDirectory.getAbsolutePath().length(), path.length() - ".clj".length()) + ".class")
+        new File(
+          destinationDirectory.getAbsolutePath + path.substring(
+            sourceDirectory.getAbsolutePath().length(),
+            path.length() - ".clj".length()
+          ) + ".class"
+        )
       }
 
       (sourceDirectory ** "*.clj").get map (clojureClazz) foreach { file =>
-        if(file.exists())
+        if (file.exists())
           IO.delete(file)
       }
 
-      ClojureCompile.compile(classpath, sourceDirectory, stubDirectory, destinationDirectory)
+      ClojureCompile.compile(
+        classpath,
+        sourceDirectory,
+        stubDirectory,
+        destinationDirectory
+      )
     },
-//     compile in Compile <<= (compile in Compile) map { x =>
-//   clojureCompile
-//   x
-// }
-    compile in Compile := Def.sequential(compile in Compile, clojureCompile in Compile).value
+    compile in Compile := {
+      val compileAnalysis = (compile in Compile).value
+      (clojureCompile in Compile).value
+      compileAnalysis
+    }
 //     compile in Compile := ((compile in Compile) dependsOn(clojureCompile in Compile)).value
   )
 
